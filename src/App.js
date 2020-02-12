4481c46b86a4e776d6445e047f6d852e45a24976
Stiven Trujillo <strujillo1293@gmail.com>
2020-02-12 14:00:29 -0500

    MoviesContainer added to appjs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Navbar } from './components/Navbar/Navbar';
+import MoviesContainer from './containers/MoviesContainer/MoviesContainer';
 import './App.css';
 
 function App() {
   return (
     <div className="">
       <Navbar />
+      <MoviesContainer />
     </div>
   );
 }
