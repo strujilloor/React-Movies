c1aa326ae95a4c52ab0002a211d77d0327313801
Stiven Trujillo <strujillo1293@gmail.com>
2020-02-15 14:21:20 -0500

    Movie Card CSS improved

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,17 +1,43 @@
 import React from 'react'
 
 const MovieCard = (props) => {
+
+    const container = {
+        width: '255px',
+        height: '340px',
+        overflow: 'hidden',
+        // margin: '10px',
+        position: 'relative'
+    }
+    const crop = {
+        position: 'absolute',
+        left: '-100%',
+        right: '-100%',
+        top: '-100%',
+        bottom: '-100%',
+        margin: 'auto',
+        minHeight: '100%',
+        minWidth: '100%'
+    }
+
     return (
-        <div>
-            <div className="card">
-                <img src="" alt="" className="card-img-top"/>
+        <div className="col mb-4">
+            <div className="card h-100">
+                <div style={container}>
+                    <img style={crop} src={ props.image } alt="" className="card-img-top"/>
+
+                </div>
                 <div className="card-body">
                     <h5 className="card-title">
-                        Card Title
+                        { props.title }
                     </h5>
-                    <p className="card-text">
-                        Some text to show to user for the win.
+                    <p className="card-text" >
+                        <small>
+                            { props.description.substr(0,85) + '...' }
+                        </small>
                     </p>
+                    <p><b>Director:</b> { props.director }</p>
+                    <p><b>Tema:</b> { props.theme }</p>
                 </div>
                 <div className="card-footer">
                     <a href="" className="btn btn-primary">
--- src/containers/MoviesContainer/MoviesContainer.js
+++ src/containers/MoviesContainer/MoviesContainer.js
@@ -1,13 +1,38 @@
-import React from 'react'
-import MovieCard from '../../components/MovieCard/MovieCard';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import MovieCard from '../../components/MovieCard/MovieCard'
 
 const MoviesContainer = () => {
+
+    const [ movies, setMovies ] = useState([])
+
+    useEffect( () => {
+        axios.get('https://crud-movies.herokuapp.com/api/v1/getMovies')
+            .then( data => {
+                // console.log(data);
+                setMovies(data.data)
+            })
+            .catch( error => {
+                console.log(error)
+                setMovies([])
+            })
+    })
+
     return (
         <div>
             <div className="container mt-4">
                 <h3>Movies Container</h3>
-                <div className="row">
-                    <MovieCard />
+                <div className="row row-cols-4">
+                    {movies.map( ( movie ) => (
+                        <MovieCard key={ movie._id }
+                            image={ movie.image }
+                            title={ movie.title }
+                            description={ movie.description }
+                            director={ movie.director }
+                            theme={ movie.theme }
+                            year={ movie.year }
+                        />
+                    ))}
                 </div>
             </div>
         </div>
